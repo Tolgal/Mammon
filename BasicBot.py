463e94ea28195b68070b93e27cf9fa0ea1579466
Pieter-Willem <pwtrollen@hotmail.com>
2018-01-08 21:25:45 +0100

    Removed some stuff

--- BasicBot.py
+++ BasicBot.py
@@ -9,10 +9,9 @@ import key
 import random
 
 # Here you can modify the bot's prefix and description and wether it sends help in direct messages or not.
-client = Bot(description="Basic Bot by Habchy#1665", command_prefix="-", pm_help = True)
+client = Bot(description="4LAN basic Bot", command_prefix="-", pm_help = False)
 
 # This is what happens everytime the bot launches. In this case, it prints information like server count, user count the bot is connected to, and the bot id in the console.
-# Do not mess with it because the bot can break, if you wish to do so, please consult me or someone trusted.
 
 @client.event
 async def on_ready():
@@ -22,13 +21,9 @@ async def on_ready():
 	print('--------')
 	print('Use this link to invite {}:'.format(client.user.name))
 	print('https://discordapp.com/oauth2/authorize?client_id={}&scope=bot&permissions=8'.format(client.user.id))
-	print('--------')
-	print('Support Discord Server: https://discord.gg/FNNNgqb')
-	print('Github Link: https://github.com/Habchy/BasicBot')
-	print('--------')
-	print('Created by Habchy#1665')
 
-#Commmand's
+
+"""Commmand's"""
 
 # ping > pong
 @client.command()
@@ -90,11 +85,3 @@ async def game(games : str):
 #start the bot	
 client.run(key.BotKey)
 
-# Basic Bot was created by Habchy#1665
-# Please join this Discord server if you need help: https://discord.gg/FNNNgqb
-# Please modify the parts of the code where it asks you to. Example: The Prefix or The Bot Token
-# This is by no means a full bot, it's more of a starter to show you what the python language can do in Discord.
-# Thank you for using this and don't forget to star my repo on GitHub! [Repo Link: https://github.com/Habchy/BasicBot]
-
-# The help command is currently set to be Direct Messaged.
-# If you would like to change that, change "pm_help = True" to "pm_help = False" on line 9.
